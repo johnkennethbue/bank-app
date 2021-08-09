2f5d8ac741bcd2360059ab7adc0aa35dcff1b5e8
johnkennethbue <johnbue02@gmail.com>
2021-08-09 19:12:41 +0800

    Testing commit for deploying changes on the website

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, Avion School Batch 10 Students!
+          Bank App soon to deploy
         </p>
         <a
           className="App-link"
